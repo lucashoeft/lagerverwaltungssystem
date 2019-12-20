87c4f9f3885c20fb52a22acc477c86ba1e75657c
Tom <toschmidt@uni-potsdam.de>
2019-12-20 12:30:39 +0100

    FileHandler will only import valid items

--- tests/FileHandlerTest.java
+++ tests/FileHandlerTest.java
@@ -29,14 +29,14 @@ class FileHandlerTest {
             FileHandler myParser = new FileHandler();
             String content =
                     "\"Lamy Safari Füllhalter Kunststoff Umbra Feder M 1203065\",\"Füllfederhalter & Kugelschreiber\",8,\"000001\",116,15.71\n" + // from example
-                    "\n" + // empty line
-                    "\"ZIPIT \\, Wildlings\",\"Federmäppchen\",24,\"000023\",37.5,7.99\n" + // description with embedded ,
-                    "\"ZIPIT \\\\ Wildlings\",\"Federmäppchen\",24,\"000023\",37.5,7.99\n" + // description with embedded \
-                    "\"GirlZone Geschenke für Mädchen - \\\"Gelstifte Set für Mädchen\\\"\",\"Füllfederhalter & Kugelschreiber\",24,\"000026,43.2,12.99\n" + // description with embedded "
-                    "\"Staedtler 512 001 Doppelspitzer (für Bleistifte und Buntstifte Anspitzer mit Behälter)\",\"Anspitzer\",241,\"A00016\",46.4,3.56\n" + // invalid location
-                    "\"Dreikant-Buntstift - STABILO Trio dick kurz - 12er Pack - mit 12 verschiedenen Farben\",\"Stifte\",21,\"000012\",69.2,4.75\n";
+                    "\n" + // empty line -> ignore
+                    "ZIPIT, Wildlings,Federmäppchen,24,000023,37.5,7.99\n" + // description with embedded , -> invalid number of fields -> ignore
+                    "ZIPIT Wildlings,Federmäppchen,24,000023,,7.99\n" + // weight not specified -> ignore
+                    "ZIPIT Wildlings,Federmäppchen,24,000023,37.5,\n" + // price not specified -> ignore
+                    "\"GirlZone Geschenke für Mädchen - \"Gelstifte Set für Mädchen\"\",Füllfederhalter & Kugelschreiber,24,000026,43.2,12.99\n" + // description with embedded "
+                    "Dreikant-Buntstift - STABILO Trio dick kurz - 12er Pack - mit 12 verschiedenen Farben\",\"Stifte\",21,\"000012\",69.2,4.75\n";
             String fileName = TestHelpers.createTmpFileWithContent("my_test_database_", ".csv", content);
-            Assertions.assertEquals(6, myParser.readInventoryFromCSV(Paths.get(fileName)).size());
+            Assertions.assertEquals(3, myParser.readInventoryFromCSV(Paths.get(fileName)).size());
             // TODO check content of items
             TestHelpers.deleteTmpFile(fileName);
         });
